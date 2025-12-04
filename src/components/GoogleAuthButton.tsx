876cc2b180badf04eba6b3e7364fc4af939d2c53
Miguel Herrera <mherrera@MacBook-Pro-de-Miguel.local>
2025-12-04 17:31:46 -0300

    feature: add google auth button component

---
+++ src/components/GoogleAuthButton.tsx
@@ -0,0 +1,44 @@
+import React, { use, useEffect, useState } from 'react'
+
+interface GoogleAuthButtonProps {
+    googleLoginHost: string
+    googleLoginClientId: string
+    googleLoginUrl: string
+}
+
+export default function GoogleAuthButton({
+    googleLoginHost,
+    googleLoginClientId,
+    googleLoginUrl,
+}: GoogleAuthButtonProps) {
+    const [isScriptLoaded, setIsScriptLoaded] = useState(false)
+
+    useEffect(() => {
+        setIsScriptLoaded(true)
+    }, [])
+
+    return (
+
+        <>{
+            isScriptLoaded ? <div className="btn-group mb-3 d-flex flex-row justify-content-center" role="group" aria-label="Google login">
+                <script src={googleLoginHost} async></script>
+                <div id="g_id_onload"
+                    data-testid="g_id_onload"
+                    data-client_id={googleLoginClientId}
+                    data-login_uri={googleLoginUrl}
+                    data-auto_prompt="false">
+                </div>
+                <div className="g_id_signin justify-content-center text-center"
+                    data-type="standard"
+                    data-size="large"
+                    data-theme="outline"
+                    data-text="sign_in_with"
+                    data-shape="rectangular"
+                    data-logo_alignment="left">
+                </div>
+            </div> : <></>
+        }
+        </>
+    )
+
+}
