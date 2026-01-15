3e6b3a65f6920fc1625362b2f7068c4dead20311
Miguel Herrera <mherrera@MacBook-Pro-de-Miguel.local>
2026-01-15 16:38:16 -0300

    fix: remove code smells

--- src/components/Copyable.tsx
+++ src/components/Copyable.tsx
@@ -30,7 +30,7 @@ export function Copyable({ text, copyIcon, checkIcon, copiedText = 'Copied!', fa
     const handleCopy = async () => {
         await navigator.clipboard.writeText(text)
         setIsCopied(true)
-        setTimeout(() => setIsCopied(false), fadeOutTime)
+        setTimeout(() => { setIsCopied(false) }, fadeOutTime)
     }
 
     const handleMouseEnter = () => {
@@ -45,7 +45,7 @@ export function Copyable({ text, copyIcon, checkIcon, copiedText = 'Copied!', fa
         return className !== null && className !== undefined
     }
 
-    const copy = (copyIcon && isValid(copyIcon)) ? <i className={copyIcon} role='img' aria-label='Copy Icon' style={copyIconStyle} onClick={handleCopy} /> : <span style={textStyle} onClick={handleCopy}>Copy</span>
+    const copy = (copyIcon && isValid(copyIcon)) ? <i className={copyIcon} role='img' aria-label='Copy Icon' style={copyIconStyle} onClick={async () => { await handleCopy() }} /> : <span style={textStyle} onClick={async () => { await handleCopy() }}>Copy</span>
     const check = (checkIcon && isValid(checkIcon)) ? <span style={textStyle}><i className={checkIcon} role='img' aria-label='Check Icon' style={copyIconStyle} /></span> : <span style={textStyle}>{copiedText}</span>
 
     return (
